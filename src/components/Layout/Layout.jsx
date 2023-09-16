7186a56f46acb758326f991fbcbcc1c7d2b60338
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-09-16 21:02:27 +0300

    1

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <>
       <Header>
-        <Filmotela to={'/'}>Filmotela</Filmotela>
+        <Filmotela to={'/'}>Filmoteka</Filmotela>
         <nav>
           <Ul>
             <li>
